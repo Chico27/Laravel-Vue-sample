7bf0c590bfbbeab7c25ecfc7df612d661e1512a2
chico27 <chico27@chico-shikaku.com>
2020-03-02 12:29:09 +0900

    init

---
+++ laraVue-docker-compose/var/www/backend/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Models\Contact;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        factory(Contact::class, 30)->create();
+    }
+}
